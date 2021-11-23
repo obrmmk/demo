1eb80a5304882b82baeff2b9862952b605905d0f
Momoka Obara <cndyxxcndy@gmail.com>
2021-11-23 23:19:41 +0900

    updated

--- pynmt/__init__.py
+++ pynmt/__init__.py
@@ -4,8 +4,8 @@ from transformers import MT5ForConditionalGeneration, MT5Tokenizer
 
 from google_drive_downloader import GoogleDriveDownloader
 
-# デバイスの指定
-DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+USE_GPU = torch.cuda.is_available()
+DEVICE = torch.device('cuda:0' if USE_GPU else 'cpu')
 print('DEVICE :', DEVICE)
 
 
@@ -15,6 +15,8 @@ class NMT(object):
     model: object
     def __init__(self, dir):
         self.trained_model = MT5ForConditionalGeneration.from_pretrained(dir).to(DEVICE)
+        if USE_GPU:
+            trained_model.cuda()
         self.tokenizer = MT5Tokenizer.from_pretrained(dir, is_fast=True)
         additional_special_tokens = ['<A>', '<B>', '<C>', '<D>', '<E>', '<a>', '<b>', '<c>', '<d>', '<e>']
         self.tokenizer.add_tokens(additional_special_tokens)
@@ -22,6 +24,8 @@ class NMT(object):
     def translate_beam(self, src_sentence: str):
         self.trained_model.config.update({"num_beams": 5})
         input_ids = self.tokenizer(src_sentence, return_tensors='pt').input_ids
+        if USE_GPU:
+            input_ids = input_ids.cuda()
         predict = self.trained_model.generate(input_ids,
                          return_dict_in_generate=True,
                          output_scores=True,
