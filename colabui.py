f8e3aa1c31ab3193b21c391a9bfefc69add6d89b
Momoka Obara <cndyxxcndy@gmail.com>
2021-11-24 23:17:10 +0900

    updated

--- colabui.py
+++ colabui.py
@@ -100,9 +100,9 @@ logger_test()
 def start_translator(translate=dummy, html=TRANSLATOR_HTML):
     def convert(text):
         try:
-            logger.info('source:', text)
+            logger.info(text)
             text = translate(text)
-            logger.info('target:', text)
+            logger.info(text)
             return IPython.display.JSON({'result': text})
         except Exception as e:
             print(e)
