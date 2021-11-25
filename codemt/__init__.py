568d3096bdfc31fdae0c0596f9239f43b908d9cb
Momoka Obara <cndyxxcndy@gmail.com>
2021-11-25 11:15:15 +0900

    updated

--- codemt/__init__.py
+++ codemt/__init__.py
@@ -72,7 +72,7 @@ def print_nop(*s):
 
 from slackweb import Slack        
 
-def make_codemt(nmt=dummy, print=print_nop, slack_url):
+def make_codemt(slack_url, nmt=dummy, print=print_nop):
     def translate(s, **kw):
         ss = []
         for statement, options in deeppy(s):
