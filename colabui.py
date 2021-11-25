a5c23820f4177b9f0a778492293cc056fe3ba9aa
Momoka Obara <cndyxxcndy@gmail.com>
2021-11-25 11:00:45 +0900

    updated

--- colabui.py
+++ colabui.py
@@ -78,10 +78,10 @@ def start_translator(translate=dummy, html=TRANSLATOR_HTML):
     def convert(text):
         try:
             logger.info(text)
-            slack.notify(text = s)
+            slack.notify(text = text)
             text = translate(text)
             logger.info(text)
-            slack.notify(text = s)
+            slack.notify(text = text)
             return IPython.display.JSON({'result': text})
         except Exception as e:
             print(e)
