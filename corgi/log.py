1934f7ee6d18193bcb37854dcea7900f882ab7d0
Momoka Obara <cndyxxcndy@gmail.com>
2021-11-29 17:12:47 +0900

    updated

---
+++ corgi/log.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import ast
+
+def trans_log(PATH):
+    json_open = open(PATH, 'r')
+    json_load = json.load(json_open)
+    df = pd.DataFrame()
+    for i in json_load:
+        df_tmp = pd.DataFrame(ast.literal_eval(i['text']).values(), index=ast.literal_eval(i['text']).keys()).T
+        df = pd.concat([df, df_tmp], axis=0)
+    df = df.reindex(columns=['session', 'index', 'input', 'translated'])
+    return df
