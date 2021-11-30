0f6f746033f457e05b00a18ca1d23fe180e1c84d
Momoka Obara <cndyxxcndy@gmail.com>
2021-11-30 13:48:11 +0900

    updated

---
+++ legacy/pynmt/__init__.py
@@ -0,0 +1,62 @@
+try:
+    import sentencepiece
+except ModuleNotFoundError:
+    import os
+    os.system('pip install sentencepiece')
+
+try:
+    import slackweb
+except ModuleNotFoundError:
+    import os
+    os.system('pip install slackweb')
+
+try:
+    import transformers
+except ModuleNotFoundError:
+    import os
+    os.system('pip install transformers')
+
+import torch
+# from torch.utils.data import Dataset, DataLoader
+from transformers import MT5ForConditionalGeneration, MT5Tokenizer
+
+from google_drive_downloader import GoogleDriveDownloader
+
+USE_GPU = torch.cuda.is_available()
+DEVICE = torch.device('cuda:0' if USE_GPU else 'cpu')
+print('DEVICE :', DEVICE)
+
+
+MODEL_DIR = "./content/model/"
+
+class NMT(object):
+    model: object
+    def __init__(self, dir):
+        self.trained_model = MT5ForConditionalGeneration.from_pretrained(dir).to(DEVICE)
+        self.tokenizer = MT5Tokenizer.from_pretrained(dir, is_fast=True)
+
+    def translate_beam(self, src_sentence: str):
+        self.trained_model.config.update({"num_beams": 5})
+        input_ids = self.tokenizer(src_sentence, return_tensors='pt').input_ids.to(DEVICE)
+        predict = self.trained_model.generate(input_ids,
+                         return_dict_in_generate=True,
+                         output_scores=True,
+                         length_penalty = 5,
+                         num_return_sequences = 5,
+                         early_stopping = True)
+        pred_list = sorted([[self.tokenizer.decode(predict.sequences[i], skip_special_tokens=True), predict.sequences_scores[i].item()] for i in range(len(predict))], key=lambda x:x[1], reverse=True)
+        sentences_list = [ i[0] for i in pred_list ]
+        scores_list = [ i[1] for i in pred_list ]                   
+        return sentences_list, scores_list
+
+def make_pynmt(model_id='1qZmBK0wHO3OZblH8nabuWrrPXU6JInDc', model_file='./model.zip'):
+    GoogleDriveDownloader.download_file_from_google_drive(
+        file_id=model_id, dest_path=model_file, unzip=True)
+    nmt = NMT(MODEL_DIR)
+
+    def pynmt(sentence):
+        pred, prob = nmt.translate_beam(sentence)
+        return pred, prob
+
+    return pynmt
+
---
+++ legacy/samoyed/__init__.py
@@ -0,0 +1,115 @@
+import os
+import sys
+import pegtree as pg
+sys.path.append(os.path.dirname(__file__))
+from transcompiler import TransCompiler
+
+class Samoyed(TransCompiler):
+    parser: object
+
+    def __init__(self, grammar_file='samoyed.pegtree'):
+        TransCompiler.__init__(self)
+        peg = pg.grammar(grammar_file)
+        self.parser = pg.generate(peg)
+
+    def generate(self, source: str, display=None):
+        tree = self.parser(source)
+        if display is not None:
+            display(tree)
+        code = self.visit(tree)
+        return repr(code)
+
+try:
+    from IPython.display import SVG, display
+    from IPython.core.magic import register_cell_magic
+    import pegtree.graph as graph
+
+    def display_tree(tree):
+        v = graph.draw_graph(tree)
+        v.format = 'SVG'
+        display(SVG(v.render()))
+
+    @register_cell_magic
+    def samoyed(option: str, source: str):
+        cc = Samoyed()
+        display = display_tree if '--tree' in option else None
+        code = cc.generate(source, display=display)
+        if '--code' in option: # ソースコードを表示する
+            print(code)
+        # 実行する
+        try:
+            ipy = get_ipython()
+            expr = ipy.input_transformer_manager.transform_cell(code)
+            expr_ast = ipy.compile.ast_parse(expr)
+            expr_ast = ipy.transform_ast(expr_ast)
+            executable = ipy.compile(expr_ast, '', 'exec')
+            exec(executable)
+        except Exception as e:
+            print(repr(code))
+            print('エラー検出', e.__class__.__name__, repr(e))
+            raise
+
+except:
+    pass
+
+def samoyed_cc(source: str):
+    cc = Samoyed()
+    return cc.generate(source)
+
+def demo():
+    import IPython
+    from google.colab import output
+
+    def convert(text):
+        with output.redirect_to_element('#output'):
+            text = samoyed_cc(text)
+            display(IPython.display.HTML(f'<textarea style="width: 48%; height:100px">{text}</textarea>'))
+    
+    output.register_callback('notebook.Convert', convert)
+
+    display(IPython.display.HTML('''
+    <textarea id="input" style="float: left; width: 48%; height:100px"></textarea>
+    <div id="output">
+    <textarea style="width: 48%; height:100px"></textarea>
+    </div>
+    <script>
+      var timer = null;
+      document.getElementById('input').addEventListener('input', (e) => {
+        var text = e.srcElement.value;
+        if(timer !== null) {
+          console.log('clear');
+          clearTimeout(timer);
+        }
+        timer = setTimeout(() => {
+          var parent = document.getElementById('output');
+          parent.innerHTML='';
+          google.colab.kernel.invokeFunction('notebook.Convert', [text], {});
+          timer = null;
+        }, 1000);
+      });
+    </script>
+    '''))
+
+
+def main(argv):
+    cc = Samoyed()
+    if len(argv) == 0:
+        import readline
+        try:
+            while True:
+                line = input('>>> ')
+                if line == '':
+                    print('Bye')
+                    sys.exit(0)
+                code = cc.generate(line)
+                print(code)
+        except EOFError:
+            pass
+    else:
+        filename = argv[0]
+        with open(filename) as f:
+            code = cc.generate(''.join(f.readlines()))
+        print(code)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
