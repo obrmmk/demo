ca79295dac336a49e8f14ca545280b74c822cfe7
kkuramitsu <kkuramitsu@gmail.com>
2021-12-11 01:14:55 +0900

    quantization

--- corgi/t5.py
+++ corgi/t5.py
@@ -20,24 +20,38 @@ print('DEVICE :', DEVICE)
 MODEL_DIR = "./content/model/"
 
 
+def quantize_transform(model):
+    model = torch.quantization.quantize_dynamic(
+        model, {torch.nn.Linear}, dtype=torch.qint8
+    )
+    return model
+
+
 class NMT(object):
     model: object
 
     def __init__(self, dir):
-        self.trained_model = MT5ForConditionalGeneration.from_pretrained(
-            dir).to(DEVICE)
+        self.trained_model = quantize_transform(MT5ForConditionalGeneration.from_pretrained(
+            dir).to(DEVICE))
         self.tokenizer = MT5Tokenizer.from_pretrained(dir, is_fast=True)
         # additional_special_tokens = ['<A>', '<B>', '<C>', '<D>', '<E>', '<a>', '<b>', '<c>', '<d>', '<e>']
         # self.tokenizer.add_tokens(additional_special_tokens)
 
-    def translate_beam(self, src_sentence: str, beams: int):
+    def translate_beam(self, src_sentence: str, beams: int, max_length=64):
         self.trained_model.config.update({"num_beams": beams})
-        input_ids = self.tokenizer(
-            src_sentence, return_tensors='pt').input_ids.to(DEVICE)
+        input_ids = self.tokenizer.encode_plus(
+            src_sentence,
+            add_special_tokens=True,
+            max_length=max_length,
+            # padding="max_length",
+            padding="do_not_pad",
+            truncation=True,
+            return_tensors='pt').input_ids.to(DEVICE)
         predict = self.trained_model.generate(input_ids,
                                               return_dict_in_generate=True,
                                               output_scores=True,
-                                              length_penalty=5,
+                                              length_penalty=8.0,
+                                              max_length=max_length,
                                               num_return_sequences=beams,
                                               early_stopping=True)
         pred_list = sorted([[self.tokenizer.decode(predict.sequences[i], skip_special_tokens=True),
@@ -47,14 +61,14 @@ class NMT(object):
         return sentences_list, scores_list
 
 
-def generate_nmt(model_id='1qZmBK0wHO3OZblH8nabuWrrPXU6JInDc', model_file='./model.zip'):
+def generate_nmt(model_id='1qZmBK0wHO3OZblH8nabuWrrPXU6JInDc', model_file='./model.zip', max_length=128):
     if not os.path.exists(MODEL_DIR):
         GoogleDriveDownloader.download_file_from_google_drive(
             file_id=model_id, dest_path=model_file, unzip=True)
     nmt = NMT(MODEL_DIR)
 
     def nmt_t5(sentence, beams=5):
-        pred, prob = nmt.translate_beam(sentence, beams)
+        pred, prob = nmt.translate_beam(sentence, beams, max_length)
         return pred, prob
 
     return nmt_t5
